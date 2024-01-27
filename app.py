9f847e66817088e24edb9c2e68ddefa26f848b6e
Manuel <manuel14.provenzi@gmail.com>
2024-01-27 18:00:56 +0100

    grafico iniziale

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from aplhavantage import AP
+from secret import API_KEY
+import matplotlib.pyplot as plt
 #look polygon.io
 def main():
-    pass
+    ap = AP(API_KEY)
+    
+    
+    #menu cmd
+    giorni = input("di quanti giorni vuoi il grafico? ")
+    ticker = input("di che ticker? ")
+    
+    
+    arrValori,arrDate = ap.getTickerList(ticker,int(giorni))
+
+    # Crea il grafico
+    plt.plot(arrDate, arrValori, marker='o', linestyle='-', color='b')
+
+    # Aggiungi etichette agli assi e un titolo
+    plt.xlabel('Date')
+    plt.ylabel('Valori più alti')
+    plt.title("ticker:"+ticker)
+
+    # Aggiungi una griglia
+    plt.grid(True)
+
+    # Rotazione delle etichette sull'asse x per una migliore leggibilità
+    plt.xticks(rotation=45)
+
+    # Mostra il grafico
+    plt.show()
+    
 
 if __name__=="__main__":
     main()
