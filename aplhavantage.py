1a82075b86ab3c678090dba3697a0052619e94ab
Manuel <manuel14.provenzi@gmail.com>
2024-01-27 11:51:38 +0100

    creazione classe

---
+++ aplhavantage.py
@@ -0,0 +1,7 @@
+class AP:
+    api_key="demo"
+    def __init__(self,api):
+        self.api_key=api
+        pass
+    def fuinc(self):
+        pass
--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-import matplotlib
+from aplhavantage import AP
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
