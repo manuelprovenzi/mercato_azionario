a30e4e1add116eac7400664b0425a8139404054e
Manuel <manuel14.provenzi@gmail.com>
2024-02-01 21:48:27 +0100

    all data got from  api get call

--- aplhavantage.py
+++ aplhavantage.py
@@ -1,5 +1,12 @@
 import requests
 from datetime import datetime, timedelta
+
+#"1. open": "187.7100",
+#2. high": "188.6500",
+# "3. low": "186.7700"
+#"4. close": "187.8700",
+#"5. volume": "4575058"
+
 class AP:
     api_key="demo"
     def __init__(self,api):
@@ -23,8 +30,13 @@ class AP:
 
             # Verifica se la data esiste nel tuo JSON
             if current_date_str in time_series_daily:
-                valore = float(time_series_daily[current_date_str]["2. high"])
-                arrValue.append("{:.2f}".format(valore))
+                open = float(time_series_daily[current_date_str]["1. open"])
+                high = float(time_series_daily[current_date_str]["2. high"])
+                low = float(time_series_daily[current_date_str]["3. low"])
+                close = float(time_series_daily[current_date_str]["4. close"])
+                
+                
+                arrValue.append(("{:.2f}".format(open),"{:.2f}".format(high),"{:.2f}".format(low),"{:.2f}".format(close)))
                 
                 arrDate.append(current_date)
                 
